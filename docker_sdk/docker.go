b39daa9c9d4f91cf5282ca5a4f9005e80ddcbb3d
shullbaz <152217267+shullbaz@users.noreply.github.com>
2025-10-31 09:34:09 -0700

    Container Stop, Delete(#5)
    * Mixed randomizeName
    
    * Initial creation
    
    * Added close and remove functions and mades some minor tweeks.
    
    * Added last little bit

--- docker_sdk/docker.go
+++ docker_sdk/docker.go
@@ -3,21 +3,22 @@ package docker_sdk
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
+	"time"
+
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
 	"github.com/pkg/errors"
-	"io"
-	"os"
-	"time"
 )
 
-func StartContainer(img string, ctx context.Context, ctrName string) error {
+func StartContainer(img string, ctx context.Context, ctrName string) (string, error) {
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		return errors.Errorf("Failed to create client: %v", err)
+		return "", errors.Errorf("Failed to create client: %v", err)
 	}
 
 	// pull image if not present
@@ -25,7 +26,7 @@ func StartContainer(img string, ctx context.Context, ctrName string) error {
 	// image.PullOptions are vars for how pulling occurs
 	pullResponse, err := cli.ImagePull(ctx, img, types.ImagePullOptions{})
 	if err != nil {
-		return errors.Errorf("Failed to pull image: %v", err)
+		return "", errors.Errorf("Failed to pull image: %v", err)
 	}
 	defer pullResponse.Close()
 
@@ -33,7 +34,7 @@ func StartContainer(img string, ctx context.Context, ctrName string) error {
 	// TODO: confirm if this is needed
 	_, err = io.Copy(os.Stdout, pullResponse)
 	if err != nil {
-		return errors.Errorf("Failed to pull image and flush io: %v", err)
+		return "", errors.Errorf("Failed to pull image and flush io: %v", err)
 	}
 
 	// port bindings!
@@ -46,6 +47,9 @@ func StartContainer(img string, ctx context.Context, ctrName string) error {
 		"5900/tcp": []nat.PortBinding{{HostIP: "localhost", HostPort: "5900"}},
 	}
 
+	// Generate the randomized name and store it
+	actualName := randomizeName(ctrName)
+
 	// Create container
 	containerResponse, err := cli.ContainerCreate(ctx,
 		&container.Config{
@@ -57,25 +61,77 @@ func StartContainer(img string, ctx context.Context, ctrName string) error {
 		},
 		&network.NetworkingConfig{},
 		nil,
-		randomizeName(ctrName),
+		actualName,
 	)
 	if err != nil {
-		return errors.Errorf("Failed to create container: %v", err)
+		return "", errors.Errorf("Failed to create container: %v", err)
 	}
 
 	// Start the container!
 	if err := cli.ContainerStart(ctx, containerResponse.ID, container.StartOptions{}); err != nil {
-		return errors.Errorf("Failed to start container: %v", err)
+		return "", errors.Errorf("Failed to start container: %v", err)
 	}
 
 	fmt.Printf("Started container %s", img)
 
-	return nil
+	return actualName, nil
 }
 
 // randomize names for containers to create unique options
 func randomizeName(ctrName string) string {
 	timestamp := time.Now().Unix()                // seconds since epoch
 	last4 := fmt.Sprintf("%04d", timestamp%10000) // get last 4 digits
-	return s + last4
+	return ctrName + last4
+}
+
+func StopContainer(ctx context.Context, ctrName string) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return errors.Errorf("Failed to create Docker client: %v", err)
+	}
+
+	// Attempt to stop the container, using the standard stop time
+	err = cli.ContainerStop(ctx, ctrName, container.StopOptions{})
+	if err != nil {
+		return errors.Errorf("Failed to stop container %s: %v", ctrName, err)
+	}
+
+	fmt.Printf("Stopped container %s\n", ctrName)
+	return nil
+}
+
+func RemoveContainer(ctx context.Context, ctrName string, force bool) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return errors.Errorf("Failed to create Docker client: %v", err)
+	}
+
+	// Check if container exists and is stopped before removing
+	// Should add a call to stop container in this if container running?
+	if !force {
+		containerInfo, err := cli.ContainerInspect(ctx, ctrName)
+		if err != nil {
+			return errors.Errorf("Container %s not found: %v", ctrName, err)
+		}
+
+		if containerInfo.State.Running {
+			return errors.Errorf("Container %s is still running. Stop it first or use force=true", ctrName)
+		}
+	}
+
+	// Struct that manages the type to removal
+	removeOptions := types.ContainerRemoveOptions{
+		Force:         force,
+		RemoveVolumes: true,
+		RemoveLinks:   false, // explicitly set to false unless you want link removal
+	}
+
+	// Remove container
+	err = cli.ContainerRemove(ctx, ctrName, removeOptions)
+	if err != nil {
+		return errors.Errorf("Failed to remove container %s: %v", ctrName, err)
+	}
+
+	fmt.Printf("Removed container %s\n", ctrName)
+	return nil
 }
--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"context"
+	"fmt"
+
 	"github.com/tlop503/quicksand/docker_sdk"
 )
 
 func main() {
 	ctx := context.Background()
 
-	err := docker_sdk.StartContainer("jlesage/firefox", ctx, "firefox_go")
+	containerName, err := docker_sdk.StartContainer("jlesage/firefox", ctx, "firefox_go")
 	if err != nil {
 		panic(err)
+	}
+
+	err = docker_sdk.StopContainer(ctx, containerName)
+	if err != nil {
+		fmt.Printf("Error stopping container: %v\n", err)
+	} else {
+		fmt.Println("Container stopped successfully")
+	}
+
+	// Remove the container (using force=true in case stop failed)
+	err = docker_sdk.RemoveContainer(ctx, containerName, false)
+	if err != nil {
+		fmt.Printf("Error removing container: %v\n", err)
+	} else {
+		fmt.Println("Container removed successfully")
 	}
 }
